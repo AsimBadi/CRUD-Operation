8e90e439ee774351b0072b301588060796f6b060
AsimBadi <asimbadi72@gmail.com>
2024-08-28 15:00:24 +0530

    Edit the View Part

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,11 +1,11 @@
-<p><strong>Id:</strong> {{$student->id}}</p>
-<p><strong>Name:</strong> {{$student->name}}</p>
-<p><strong>Email:</strong> {{$student->email}}</p>
-<p><strong>Description:</strong> {{$student->description}}</p>
-<p><strong>Machines:</strong> {{$student->machines}}</p>
-<p><strong>Gender:</strong> {{$student->gender}}</p>
-<p><strong>Country:</strong> {{$student->country}}</p>
-<p><strong>Category:</strong> {{$student->category->name}}</p>
-<p><strong>Created At:</strong> {{$student->created_at}}</p>
-<p><strong>Updated At:</strong> {{$student->updated_at}}</p>
+<p><strong>Id: </strong>{{$student->id}}</p>
+<p><strong>Name: </strong>{{$student->name}}</p>
+<p><strong>Email: </strong>{{$student->email}}</p>
+<p><strong>Description: </strong>{{$student->description}}</p>
+<p><strong>Machines: </strong>{{$student->machines}}</p>
+<p><strong>Gender: </strong>{{$student->gender}}</p>
+<p><strong>Country: </strong>{{strtoupper($student->country)}}</p>
+<p><strong>Category: </strong>{{$student->category->name}}</p>
+<p><strong>Created at: </strong>{{$student->created_at}}</p>
+<p><strong>Updated at: </strong>{{$student->updated_at}}</p>
 <a href="/">Back To list</a>
