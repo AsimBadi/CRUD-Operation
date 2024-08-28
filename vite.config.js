cdaa8016f90542866ff37804dab80d5ad9b823c3
AsimBadi <asimbadi72@gmail.com>
2024-08-28 16:00:26 +0530

    Vite Configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel([
             'resources/js/app.js', // Add your main JS file here
-            'resources/js/first.js'
+            'resources/js/first.js',
+            'resources/js/search.js'
         ]),
     ],
 });
